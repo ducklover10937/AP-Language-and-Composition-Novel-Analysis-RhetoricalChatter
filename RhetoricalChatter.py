eb6dae5f0458202d812881f857a1330da84c7d6b
Kristie Kim <kristiekim2009@gmail.com>
2025-09-26 00:05:12 +1000

    Update RhetoricalChatter.py

--- RhetoricalChatter.py
+++ RhetoricalChatter.py
@@ -28,8 +28,8 @@ analysisDict = {
     ("rhetoric","device","rhetorical device"): "In The Catcher in the Rye, Salinger combines the use of colloquial diction, symbolism, hypophora, and anecdotes to develop a complex account of the adolescent concerns.",
     ("examples", "example", "rhetorical device examples"): '''Anecdotes are prominent in The Catcher in the Rye; for instance, it is present here: "“I can drink all night and not even show it…Once, at Whooton School, this
     other boy, Raymond Goldfarb, and I bought a pint of Scotch and drank it in the chapel one Saturday night, where nobody’d see us. He got stinking, but I hardly didn’t even show it…I puked before I went to bed, but I 
-    didn’t really have to—I forced myself” (Salinger 118).'''
-    ("why", "rhetorical analysis", "analysis", "analyze", "analyze the example", "meaning"): """ Holden is contradictory. He wants to appear "older" through crude language, smoking, rebelling, and drinking alcoholic beverages but is hidden behind immaturity when he disparages his peers. As adulthood is often depicted in young people's mind as drinking, smoking, or vulgarity, Salinger carries out the teenaged perspective of "maturity" by conveying Holden's self-exploration, self-discovery, and coming-of-age, which contrasts with genuine adulthood of self-understanding and acceptance."""
+    didn’t really have to—I forced myself” (Salinger 118).''',
+    ("why", "rhetorical analysis", "analysis", "analyze", "analyze the example", "meaning"): """Holden is contradictory. He wants to appear "older" through crude language, smoking, rebelling, and drinking alcoholic beverages but is hidden behind immaturity when he disparages his peers. As adulthood is often depicted in young people's mind as drinking, smoking, or vulgarity, Salinger carries out the teenaged perspective of "maturity" by conveying Holden's self-exploration, self-discovery, and coming-of-age, which contrasts with genuine adulthood of self-understanding and acceptance."""
 }
 
 if "chatHistory" not in st.session_state:
@@ -54,10 +54,11 @@ if st.button("Send") and chat.strip() != "":
 
 with chatContainer: #chat history
     for chatEntry in st.session_state.chatHistory:        
-        if "The Catcher in the Rye by J.D. Salinger explores the coming-of-age" in chatEntry or "The Catcher in the Rye explores the loss of childhood innocence" in chatEntry or "In The Catcher in the Rye, Salinger combines the use of colloquial diction," in chatEntry or errorMsg in chatEntry or "Anecdotes are prominent in The Catcher in the Rye" in chatEntry or :
+        if "The Catcher in the Rye by J.D. Salinger explores the coming-of-age" in chatEntry or "The Catcher in the Rye explores the loss of childhood innocence" in chatEntry or "In The Catcher in the Rye, Salinger combines the use of colloquial diction," in chatEntry or errorMsg in chatEntry or "Anecdotes are prominent in The Catcher in the Rye" in chatEntry or "Holden is contradictory." in chatEntry:
             st.markdown("<div style='background-color: #61503b; color: #f7ecd5; text-align: left; overflow-wrap:break-word; display:inline-block; padding: 10px; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
         else:
             st.markdown("<div style='background-color: #fffbd4; color: #332815; text-align: left; overflow-wrap:break-word; float: right;display:inline-block; padding: 10px; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
+
 
 
 
