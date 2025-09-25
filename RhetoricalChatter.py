9dc8d71f0e2f0c0a3c70350ee2bc60cbab3059e7
Kristie Kim <kristiekim2009@gmail.com>
2025-09-25 23:00:43 +1000

    Update RhetoricalChatter.py

--- RhetoricalChatter.py
+++ RhetoricalChatter.py
@@ -9,7 +9,7 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-st.markdown("""<style>.stApp { background-color: #ccc5a1; color: #332815; } </style>""", unsafe_allow_html=True)
+st.markdown("""<style>.stApp { background-color: #a6a085; color: #332815; } </style>""", unsafe_allow_html=True)
 st.markdown("""<style>.stButton { background-color: #fffbd4; color: #c9924f; }</style>""", unsafe_allow_html=True)
 st.markdown("<h1 style = 'text-align: center;'>RhetoricalChatter</h1>", unsafe_allow_html=True)
 st.markdown("<p style = 'text-align: center;'>Learn about <i>The Catcher in the Rye</i> with RhetoricalChatter!</p>", unsafe_allow_html=True)
@@ -18,7 +18,7 @@ st.markdown("<p style = 'text-align: center;'>Learn about <i>The Catcher in the 
 errorMsg = "I'm sorry, I couldn't understand your request."
 
 chatContainer = st.container()
-chat = st.text_input("What would you like to learn about The Catcher in the Rye?", value="", key="chatInput", """<style>.stTextInput {color: #332815; }</style>""",unsafe_allow_html=True)
+chat = st.text_input("What would you like to learn about The Catcher in the Rye?", value="", key="chatInput")
 
 analysisDict = {
     ("introduce", "introduction", "summary", "summarize", "overview"): "The Catcher in the Rye by J.D. Salinger explores the coming-of-age experience through the "
@@ -54,6 +54,7 @@ with chatContainer: #chat history
             st.markdown("<div style='background-color: #61503b; color: #f7ecd5; text-align: left; overflow-wrap:break-word; display:inline-block; padding: 10px; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
         else:
             st.markdown("<div style='background-color: #fffbd4; color: #332815; text-align: left; overflow-wrap:break-word; float: right;display:inline-block; padding: 10px; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
+
 
 
 
