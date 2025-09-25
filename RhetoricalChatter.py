09d8a5b645192d717dee7dd5bd0cd26012d2b9c6
Kristie Kim <kristiekim2009@gmail.com>
2025-09-26 00:06:07 +1000

    Update RhetoricalChatter.py

--- RhetoricalChatter.py
+++ RhetoricalChatter.py
@@ -58,14 +58,3 @@ with chatContainer: #chat history
             st.markdown("<div style='background-color: #61503b; color: #f7ecd5; text-align: left; overflow-wrap:break-word; display:inline-block; padding: 10px; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
         else:
             st.markdown("<div style='background-color: #fffbd4; color: #332815; text-align: left; overflow-wrap:break-word; float: right;display:inline-block; padding: 10px; border-radius: 20px;'>"+chatEntry+"</div>", unsafe_allow_html=True)
-
-
-
-
-
-
-
-
-
-
-
